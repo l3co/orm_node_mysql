3cbcce4db203d86e8a3fec19aa7c9547160d2828
leandro costa <lcosta.sp.br@gmail.com>
2022-06-21 08:59:17 -0300

    feat(aula1): :sparkles: create an endpoint for test

---
+++ app/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express()
+
+app.use(
+    express.json()
+)
+
+const port = process.env.PORT || 3000;
+
+app.get("/test", (_, resp) => {
+    resp
+        .status(200)
+        .send({ "message": "Bem vindo a API" })
+})
+
+app.listen(port, () => {
+    console.log(`App started on port : ${port}`);
+})
+
+export default app
