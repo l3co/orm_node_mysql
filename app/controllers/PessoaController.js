ba3f4f6c7947fd33e32e0628b4e4d95a2f0dc9a9
leandro costa <lcosta.sp.br@gmail.com>
2022-06-25 11:51:23 -0300

    feat(aula4): :sparkles: finish crud

--- app/controllers/PessoaController.js
+++ app/controllers/PessoaController.js
@@ -6,8 +6,50 @@ class PessoaController {
 
         try {
             const pessoas = await Pessoas.findAll()
-            console.log(pessoas);
             return res.status(200).send(pessoas)
+        } catch (error) {
+            return res.status(500).send({ "message": error.message })
+        }
+    }
+
+    static async buscarPeloID(req, res) {
+        try {
+            const { id } = req.params;
+            const pessoa = await Pessoas.findOne({ where: { id: id } })
+            return res.status(200).send(pessoa)
+        } catch (error) {
+            return res.status(500).send({ "message": error.message })
+        }
+    }
+
+    static async apagar(req, res) {
+        try {
+            const { id } = req.params;
+            await Pessoas.destroy({ where: { id: id } })
+            return res.status(200).send({ "message": "Registro apagado com sucesso" })
+        } catch (error) {
+            return res.status(500).send({ "message": error.message })
+        }
+    }
+
+    static async atualizar(req, res) {
+        const { id } = req.params;
+        const updatePessoa = req.body;
+        try {
+            const query = { where: { id: id } }
+            await Pessoas.update(updatePessoa, query)
+            const pessoa = Pessoas.findOne(query)
+            return res.status(201).send(pessoa)
+        } catch (error) {
+            return res.status(500).send({ "message": error.message })
+        }
+    }
+
+    static async criar(req, res) {
+        const novaPessoa = req.body;
+        try {
+            const pessoa = await Pessoas.create(novaPessoa)
+            return res.status(201).send(pessoa)
         } catch (error) {
             return res.status(500).send({ "message": error.message })
         }
--- app/routes/pessoaRouter.js
+++ app/routes/pessoaRouter.js
@@ -4,5 +4,9 @@ const PessoaController = require("../controllers/PessoaController")
 const router = Router()
 
 router.get("/pessoas", PessoaController.listarPessoas)
+router.get("/pessoas/:id", PessoaController.buscarPeloID)
+router.delete("/pessoas/:id", PessoaController.apagar)
+router.post("/pessoas", PessoaController.criar)
+router.put("/pessoas/:id", PessoaController.atualizar)
 
 module.exports = router
