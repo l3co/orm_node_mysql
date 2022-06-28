1c87f89e6e65b330a30a3cc02982d97113f52245
leandro costa <lcosta.sp.br@gmail.com>
2022-06-28 10:14:19 -0300

    feat(aula6): :sparkles: map new routes and controllers

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,14 @@
 const express = require("express")
 const pessoaRouter = require("./pessoaRouter")
+const matriculaRouter = require("./matriculasRouter")
+const nivelRouter = require("./niveisRouter")
+const turmaRouter = require("./turmasRouter")
 
 module.exports = (app) => {
     app.use(express.json())
     app.use(pessoaRouter)
+
+    app.use(matriculaRouter)
+    app.use(nivelRouter)
+    app.use(turmaRouter)
 }
